74caac6f99049921ae9df73974805c6b1b806be0
James Turner <mail.jt@gmail.com>
2017-09-09 17:22:42 +0100

    added #Addmove

--- lib/board.rb
+++ lib/board.rb
@@ -6,4 +6,12 @@ class Board
   def board
     @board.clone
   end
+
+  def addMove player: nil, location: nil
+    raise "player not provided" if player == nil
+    raise "location not provided" if location == nil
+    raise "location out of range" if location > 8 || location < 0
+    @board[location] = player
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,6 +2,8 @@ require 'board'
 
 describe Board do
   let(:empty_board){['0','1','2','3','4','5','6','7','8']}
+  let(:player_one){double('player_one')}
+
   describe '#creation' do
     it 'starts with 9 empty squares' do
       expect(subject.board).to eq empty_board
@@ -11,8 +13,28 @@ describe Board do
   describe '#board' do
     it 'returns an array that doesn\'t change the original' do
       board = subject.board
-      board[0] = "sausages"
+      board[0] = 'sausages'
       expect(subject.board).to eq empty_board
+    end
+  end
+
+  describe '#addMove' do
+    it 'stores a reference to the player in a slot' do
+      subject.addMove(player: player_one, location: 0)
+      expect(subject.board[0]).to eq player_one
+    end
+
+    it 'requires a location' do
+      expect {subject.addMove(player: player_one)}.to raise_error 'location not provided'
+    end
+
+    it 'requires a player' do
+      expect {subject.addMove(location: 0)}.to raise_error 'player not provided'
+    end
+    it 'won\'t allow a location above 8 or below 0' do
+      error = 'location out of range'
+      expect{subject.addMove(player: player_one, location: -1)}.to raise_error error
+      expect{subject.addMove(player: player_one, location: 9)}.to raise_error error
     end
   end
 end
