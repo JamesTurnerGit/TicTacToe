43fc460479e2da404fe900332affe8c5e4fe651a
James Turner <mail.jt@gmail.com>
2017-09-09 19:49:50 +0100

    changed to an x,y system to allow for future grid expansion

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,12 @@
 class Board
 
-  def initialize (empty_symbol: "", size: 9)
+  def initialize (empty_symbol: "", size: 3)
     @size = size - 1
     @empty_symbol = empty_symbol
-    @board = [nil] * size
+    @board = []
+    size.times do
+      @board << [nil] * size
+    end
   end
 
   def board
@@ -13,23 +16,26 @@ class Board
   def addMove player: nil, location: nil
     raise 'move player not provided' if player == nil
     raise 'move location not provided' if location == nil
-    raise 'move location out of range' if locationInRange? location
-    return false if board[location]
-    @board[location] = player
+    raise 'move location out of range' unless validLocation? location
+    return false if board[location[0]][location[1]]
+    @board [location[0]][location[1]] = player
     true
   end
 
   def getMark location
-    raise 'mark location out of range' if locationInRange? location
-    return empty_symbol if board[location] == nil
-    board[location].symbol
+    raise 'mark location out of range' unless validLocation? location
+    return empty_symbol if board[location[0]][location[1]] == nil
+    board[location[0]][location[1]].symbol
   end
 
   private
 
   attr_reader :empty_symbol, :size
 
-  def locationInRange? location
-    location > size || location < 0
+  def validLocation? location
+    return false unless (location.is_a? Array) && (location.length == 2)
+    location_x_valid = location[0] < size && location[0] >= 0
+    location_y_valid = location[1] < size && location[1] >= 0
+    location_x_valid && location_y_valid
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,9 @@
 require 'board'
 
 describe Board do
-  let(:empty_board){[nil,nil,nil,nil,nil,nil,nil,nil,nil]}
+  let(:empty_board){[[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]}
   let(:player_one_symbol){"X"}
+  let(:bottom_left){[0,0]}
   let(:player_one){double('player_one',symbol: player_one_symbol)}
 
   describe '#creation' do
@@ -21,8 +22,8 @@ describe Board do
 
   describe '#addMove' do
     it 'stores a reference to the player in a slot' do
-      subject.addMove(player: player_one, location: 0)
-      expect(subject.board[0]).to eq player_one
+      subject.addMove(player: player_one, location: bottom_left)
+      expect(subject.board[0][0]).to eq player_one
     end
 
     it 'requires a location' do
@@ -30,18 +31,19 @@ describe Board do
     end
 
     it 'requires a player' do
-      expect {subject.addMove(location: 0)}.to raise_error 'move player not provided'
+      expect {subject.addMove(location: [0,0])}.to raise_error 'move player not provided'
     end
 
-    it 'won\'t allow a location above 8 or below 0' do
+    it 'won\'t allow a location in a bad location' do
       error = 'move location out of range'
-      expect{subject.addMove(player: player_one, location: -1)}.to raise_error error
-      expect{subject.addMove(player: player_one, location: 9)}.to raise_error error
+      expect{subject.addMove(player: player_one, location: [-1,0])}.to raise_error error
+      expect{subject.addMove(player: player_one, location: [4 ,0])}.to raise_error error
+      expect{subject.addMove(player: player_one, location: 5)}.to raise_error error
     end
 
     it 'will only allow you to move in a empty spot' do
-      expect(subject.addMove(player: player_one, location: 0)).to equal true
-      expect(subject.addMove(player: player_one, location: 0)).to equal false
+      expect(subject.addMove(player: player_one, location: bottom_left)).to equal true
+      expect(subject.addMove(player: player_one, location: bottom_left)).to equal false
     end
 
     xit 'will only allow you to make a move if the board isn\'t won'
@@ -57,15 +59,14 @@ describe Board do
 
   describe '#getmark' do
     it "returns the mark of whatever is at that spot in the board" do
-      subject.addMove(player: player_one, location: 0)
-      expect(subject.getMark(0)).to eq player_one_symbol
-      expect(subject.getMark(1)).to eq ""
+      expect(subject.getMark(bottom_left)).to eq ""
+      subject.addMove(player: player_one, location: [0,0])
+      expect(subject.getMark(bottom_left)).to eq player_one_symbol
     end
-    
+
     it "checks range before allowing retrieval" do
       error = 'mark location out of range'
-      expect{subject.getMark(-1)}.to raise_error error
-      expect{subject.getMark(9)}.to raise_error error
+      expect{subject.getMark([-1,0])}.to raise_error error
     end
   end
 end
